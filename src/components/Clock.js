4f02c7bacc98e450d7edaefa0e41526c29c82b3d
tayutaedomo <tayutaedomo@gmail.com>
2021-02-23 01:55:25 +0900

    add: basic clock display

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default function Clock() {
-  return <div>Clock</div>;
+const useStyles = makeStyles((theme) => ({
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+}));
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.heroContent}>
+        <Container maxWidth="sm">
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </Container>
+      </div>
+    );
+  }
 }
+
+export default withStyles(useStyles)(Clock);
