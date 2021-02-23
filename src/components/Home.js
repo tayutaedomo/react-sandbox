de067cc20328228ba8381695b5d6cc47af5a7b39
tayutaedomo <tayutaedomo@gmail.com>
2021-02-23 20:04:32 +0900

    refactor: HomeCard component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import HomeCard from './HomeCard';
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
@@ -33,7 +35,6 @@ export default function Home() {
 
   return (
     <React.Fragment>
-      {/* Hero unit */}
       <div className={classes.heroContent}>
         <Container maxWidth="sm">
           <Typography
@@ -48,30 +49,13 @@ export default function Home() {
         </Container>
       </div>
       <Container className={classes.cardGrid} maxWidth="md">
-        {/* End hero unit */}
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  State and Lifecycle
-                </Typography>
-                <Typography>
-                  Ref: https://reactjs.org/docs/state-and-lifecycle.html
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  component={Link}
-                  to="/clock"
-                >
-                  View
-                </Button>
-                {/* <Link to="/clock">Go to</Link> */}
-              </CardActions>
-            </Card>
+            <HomeCard
+              headline={'State and Lifecycle'}
+              content={'Ref: https://reactjs.org/docs/state-and-lifecycle.html'}
+              to={'/clock'}
+            />
           </Grid>
         </Grid>
       </Container>
