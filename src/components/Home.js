ea6174178a365eb5020ef2f059df5431988f9dbb
tayutaedomo <tayutaedomo@gmail.com>
2021-03-04 00:09:52 +0900

    update: Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 import HomeCard from './HomeCard';
 
 const useStyles = makeStyles((theme) => ({
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
   cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
   },
 }));
 
@@ -22,19 +17,6 @@ export default function Home() {
 
   return (
     <React.Fragment>
-      <div className={classes.heroContent}>
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="textPrimary"
-            gutterBottom
-          >
-            React Sandbox
-          </Typography>
-        </Container>
-      </div>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={4}>
