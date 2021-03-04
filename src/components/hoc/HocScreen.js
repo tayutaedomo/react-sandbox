61440e2a6388dffb4e1c98ec2259d9da1c2716a0
tayutaedomo <tayutaedomo@gmail.com>
2021-03-04 21:34:30 +0900

    add: PropsLogging component

--- src/components/hoc/HocScreen.js
+++ src/components/hoc/HocScreen.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import PropsLogging from './PropsLogging';
 
 export class HocScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
   render() {
-    return <div>HOC</div>;
+    return (
+      <React.Fragment>
+        {/* <label>Count: {this.state.count}</label> */}
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Count +1
+        </button>
+        <PropsLogging foo="foo" count={this.state.count} />
+      </React.Fragment>
+    );
   }
 }
 
