8e155301361332e714c82fcd2d60fa3cb3c34feb
tayutaedomo <tayutaedomo@gmail.com>
2021-04-08 18:45:37 +0900

    add: StyledJsx component

--- src/components/styles/Styles.jsx
+++ src/components/styles/Styles.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import InlineStyles from './InlineStyles';
 import CssModules from './CssModules';
+import StyledJsx from './StyledJsx';
 
 const Styles = () => {
   return (
     <>
       <InlineStyles />
       <CssModules />
+      <StyledJsx />
     </>
   );
 };
