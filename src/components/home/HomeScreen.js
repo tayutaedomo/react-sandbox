84d3a320fcabefa87792aa733675847521aa7368
tayutaedomo <tayutaedomo@gmail.com>
2021-04-08 20:01:39 +0900

    refactor: HomeScreen component

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -11,103 +11,148 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Home() {
+const renderCard = (card) => {
+  return (
+    <Grid item xs={12} sm={6} md={4} key={card.to}>
+      <HomeCard headline={card.headline} content={card.content} to={card.to} />
+    </Grid>
+  );
+};
+
+const Home = () => {
   const classes = useStyles();
 
   return (
     <React.Fragment>
       <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'State and Lifecycle'}
-            content={'Ref: https://reactjs.org/docs/state-and-lifecycle.html'}
-            to={'/clock'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Lifting state up, before'}
-            content={'Ref: https://ja.reactjs.org/docs/lifting-state-up.html'}
-            to={'/temperature_before'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Lifting state up, after'}
-            content={'Ref: https://ja.reactjs.org/docs/lifting-state-up.html'}
-            to={'/temperature_after'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Form'}
-            content={'Ref: https://ja.reactjs.org/docs/forms.html'}
-            to={'/form'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Form material-ui'}
-            content={'Ref: https://material-ui.com/components/checkboxes/'}
-            to={'/form_materialui'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Context'}
-            content={'Ref: https://reactjs.org/docs/context.html'}
-            to={'/context'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Composition'}
-            content={
-              'Ref: https://reactjs.org/docs/composition-vs-inheritance.html'
-            }
-            to={'/composition'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Higher-Order Component'}
-            content={
-              'Ref: https://reactjs.org/docs/higher-order-components.html'
-            }
-            to={'/hoc'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Re-rendering'}
-            content={
-              <>
-                memo/useCallback/useMemo
-                <br />
-                Ref:
-                https://www.udemy.com/course/react_stepup/learn/lecture/24823308
-              </>
-            }
-            to={'/re-rendering'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Style Coding'}
-            content={
-              'Inline Styles/CSS Modules/Styled JSX/Styled components/Emotion'
-            }
-            to={'/styles'}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <HomeCard
-            headline={'Router'}
-            content={'Basic/Nested/Refactoring/Params/404'}
-            to={'/router'}
-          />
-        </Grid>
+        {cards.map((card) => {
+          return renderCard(card);
+        })}
       </Grid>
     </React.Fragment>
   );
-}
+};
+
+const cards = [
+  {
+    headline: 'State and Lifecycle',
+    content: (
+      <a
+        href="https://reactjs.org/docs/state-and-lifecycle.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/clock',
+  },
+  {
+    headline: 'Lifting state up, before',
+    content: (
+      <a
+        href="https://ja.reactjs.org/docs/lifting-state-up.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/temperature_before',
+  },
+  {
+    headline: 'Form',
+    content: (
+      <a
+        href="https://ja.reactjs.org/docs/forms.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/form',
+  },
+  {
+    headline: 'Form material-ui',
+    content: (
+      <a
+        href="https://material-ui.com/components/checkboxes/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/form_materialui',
+  },
+  {
+    headline: 'Context',
+    content: (
+      <a
+        href="https://reactjs.org/docs/context.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/context',
+  },
+  {
+    headline: 'Composition',
+    content: (
+      <a
+        href="https://reactjs.org/docs/composition-vs-inheritance.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/composition',
+  },
+  {
+    headline: 'Higher-Order Component',
+    content: (
+      <a
+        href="https://reactjs.org/docs/higher-order-components.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Reference
+      </a>
+    ),
+    to: '/hoc',
+  },
+  {
+    headline: 'Re-rendering',
+    content: (
+      <>
+        memo, useCallback, useMemo
+        <br />
+        <a
+          href="https://www.udemy.com/course/react_stepup/learn/lecture/24823308"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Reference
+        </a>
+      </>
+    ),
+    to: '/re-rendering',
+  },
+  {
+    headline: 'Style Coding',
+    content:
+      'Inline Styles, CSS Modules, Styled JSX, Styled components, Emotion',
+    to: '/styles',
+  },
+  {
+    headline: 'Router',
+    content: 'Basic, Nested, Refactoring, Params, 404',
+    to: '/router',
+  },
+];
+
+export default Home;
