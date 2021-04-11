9fcd5f0917a2b3a7feb4f6f2c5e4ddf832f18657
tayutaedomo <tayutaedomo@gmail.com>
2021-04-12 00:15:50 +0900

    add: UserCard component

--- src/components/atomic_design/AtomicDesign.jsx
+++ src/components/atomic_design/AtomicDesign.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PrimaryButton from './atoms/button/PrimaryButton';
 import SecondaryButton from './atoms/button/SecondaryButton';
 import SearchInput from './molecules/SearchInput';
+import UserCard from './organisms/user/UserCard';
+
+const user = {
+  name: 'Taro',
+  image: 'https://source.unsplash.com/NE0XGVKTmcA',
+  email: 'aaa@example.com',
+  phone: '000-9999-8888',
+  company: {
+    name: 'AAA Inc.',
+  },
+  website: 'https://google.com',
+};
 
 const AtomicDesign = () => {
   return (
@@ -10,6 +22,7 @@ const AtomicDesign = () => {
       <SecondaryButton>Search</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
---
+++ src/components/atomic_design/atoms/card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = (props) => {
+  const { children } = props;
+
+  return <SCard>{children}</SCard>;
+};
+
+const SCard = styled.div`
+  background-color: #fff;
+  box-shadow: #ddd 0px 0px 4px 2px;
+  border-radius: 8px;
+  padding: 16px;
+`;
+
+export default Card;
