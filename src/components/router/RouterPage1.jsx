604282b8cb9c207bb5eac4f4e393ac81b2f3bdbc
tayutaedomo <tayutaedomo@gmail.com>
2021-04-11 20:55:55 +0900

    add: useHistory handling

--- src/components/router/RouterPage1.jsx
+++ src/components/router/RouterPage1.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const RouterPage1 = () => {
   const arr = [...Array(100).keys()];
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push('/router/page1/detailA');
 
   return (
     <div>
@@ -10,6 +14,8 @@ const RouterPage1 = () => {
       <Link to={{ pathname: '/router/page1/detailA', state: arr }}>
         Detail A
       </Link>
+      <br />
+      <button onClick={onClickDetailA}>Detail A</button>
     </div>
   );
 };
